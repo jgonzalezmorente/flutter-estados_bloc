5821815aed0bc7b611cd231b81d09cf8d6ee483d
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-01-07 09:52:39 +0100

    Primer commit

---
+++ lib/bloc/user/user_bloc.dart
@@ -0,0 +1,31 @@
+
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+import 'package:estados/models/user.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
+
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  
+  UserBloc() : super( const UserInitialState() ) {
+
+    on<ActivateUser>( ( event, emit ) => emit( UserSetState( event.user ) ) );
+    
+    on<ChangeUserAge>( ( event, emit ) {
+      if ( !state.existUser ) return;
+      emit( UserSetState( state.user!.copyWith( edad: event.age ) ) );
+    });
+
+    on<AddProfession>( ( event, emit ) {
+      if ( !state.existUser ) return;
+      final List<String> professions = [ ...state.user!.profesiones, event.profession ];
+      emit( UserSetState( state.user!.copyWith( profesiones: professions ) ) );
+    });
+
+    on<DeleteUser>( (event, emit ) => emit( const UserInitialState() ));
+
+  }
+
+}
---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,70 @@
+import 'package:estados/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:estados/bloc/user/user_bloc.dart';
+
+
+class Pagina2Page extends StatelessWidget {
+  
+  const Pagina2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final userBloc = BlocProvider.of<UserBloc>( context, listen: false );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagina 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            MaterialButton(
+              child: const Text('Establecer Usuario', style: TextStyle( color: Colors.white ) ),
+              color: Colors.blue,
+              onPressed: () {
+
+                final newUser = User(
+                  nombre: 'Fernando',
+                  edad: 36,
+                  profesiones: [
+                    'FullStack Developer'
+                  ]
+                );
+
+                userBloc.add( ActivateUser( newUser ) );
+
+              }
+            ),
+
+            MaterialButton(
+              child: const Text('Cambiar Edad', style: TextStyle( color: Colors.white ) ),
+              color: Colors.blue,
+              onPressed: () {
+
+                userBloc.add( ChangeUserAge( 39 ) );
+
+              }
+            ),
+
+            MaterialButton(
+              child: const Text('Añadir Profesion', style: TextStyle( color: Colors.white ) ),
+              color: Colors.blue,
+              onPressed: () {
+
+                if ( userBloc.state.existUser ){
+                  userBloc.add( AddProfession( 'Profesion ${ userBloc.state.user!.profesiones.length + 1 }') );
+                }
+
+              }
+            ),
+
+          ],
+        )
+     ),
+   );
+  }
+}
